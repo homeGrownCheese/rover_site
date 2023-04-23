74c14fed46efc4b5ba2a772b7eae2d263f8c8390
homegrowncheese <shanis9er@me.com>
2023-04-22 18:28:01 -0700

    lets users add their keys

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 import requests
 import dotenv
 import os
+import json
+
 
 from django.shortcuts import render
 
@@ -12,18 +14,33 @@ API_KEY = os.getenv("API_KEY")
 
 
 def home(request):
+    # check if the API key is present in the form data
+    if "api_key" in request.GET:
+        # save the API key to a cookie
+        api_key = request.GET["api_key"]
+        response = render(request, "home/index.html")
+        response.set_cookie("api_key", api_key)
+
+    # load the API key from the cookie or use the default key
+    api_key = request.COOKIES.get("api_key", os.getenv("API_KEY"))
+
     # get the users selections for sol and camera from the form
     sol = request.GET.get("sol", 1000)
     camera = request.GET.get("camera", "fhaz")
     page = request.GET.get("page", 1)
 
     if camera == "all":
-        url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol={sol}&page={page}&api_key={API_KEY}"
+        url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol={sol}&page={page}&api_key={api_key}"
     else:
-        url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol={sol}&camera={camera}&page=1&api_key={API_KEY}"
+        url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol={sol}&camera={camera}&page=1&api_key={api_key}"
     # make a request to the NASA API
+    # print the headers
+    headers = requests.get(url).headers
+    print(
+        headers["X-RateLimit-Remaining"],
+    )
+    print(headers)
 
     data = requests.get(url).json()
-    # print(data)
-    print(API_KEY)
+
     return render(request, "home/index.html", {"data": data})
